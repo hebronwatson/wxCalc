b6ac303efabc23b6d6e178d4bb9839f0b0dab513
Hebron Watson <hebron.watson@ext.airbnb.com>
2022-11-16 23:43:33 -0800

    [wxCalc][gtest-cmake] make functions for button layout

--- test/Test.cpp
+++ test/Test.cpp
@@ -3,9 +3,14 @@
 #include "../src/SpaceFactory.cpp"
 
 TEST(RUN_TEST_SUITE, CLASS_CONSTRUCTION_TEST){
-    ScreenSpace* screenSpace = SpaceFactory::DisplayScreen();
-    ASSERT_EQ(screenSpace, nullptr);
-    delete screenSpace;
+    ScreenSpace screenSpace = SpaceFactory::DisplayScreen();
+    ScreenSpace SCREEN_SD = {
+        wxPoint(0,0),
+        wxSize(640,480)
+    };
+
+    ASSERT_EQ(screenSpace.location, SCREEN_SD.location);
+    ASSERT_EQ(screenSpace.size, SCREEN_SD.size);
 }
 
 int main(int argc, char** argv){
